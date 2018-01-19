bb82ccb586a69a76733e75e71b2d22cdc2100f79
Ravin Singh D <ravin@getcloudcherry.com>
2018-01-19 23:11:23 +0530

    init

---
+++ src/client/app.js
@@ -0,0 +1,4 @@
+import * as statusEntry from './templates/status-entry.html';
+
+const body = document.querySelector('body');
+body.innerHTML = statusEntry;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [{
+      test: /\.(html)$/,
+      use: {
+        loader: 'html-loader'
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+};
